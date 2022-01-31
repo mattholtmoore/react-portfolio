478368c0b01c4f54936f75283020a87f94e8ef04
Matthew Moore <mattholtmoore@gmail.com>
2022-01-31 15:27:07 -0500

    updated contact forms font / updated nav font / fixed contact forms error message & input

--- src/components/Contact/Form/index.js
+++ src/components/Contact/Form/index.js
@@ -26,20 +26,25 @@ function Form() {
     e.preventDefault();
 
     if (!validateEmail(email) || !name) {
-      setErrorMessage('Email or Name is invalid. Please fill out all inputs. Thanks!');
+      setErrorMessage('*Email or Name is invalid. Please fill out all inputs. Thanks!');
       return;
+    } else {
+      setErrorMessage('');
     }
 
-    setName('');
-
-    setEmail('');
     alert(`Hey, ${name}! So nice to meet you.`);
+    setName('');
+    setMessage('');
+    setEmail('');
   };
 
   return (
     <div className="container-fluid p-3">
       <p>Let's Connect!</p>
       <form className="form p-3">
+        <h4
+          className="text-light mt-4">Name:
+        </h4>
         <input
           className="fs-5 border border-info p-2"
           value={name}
@@ -48,6 +53,7 @@ function Form() {
           type="text"
           placeholder="What's your name?"
         />
+        <h4 className="text-light mt-4">Email:</h4>
         <input
           className="fs-5 border border-info p-2"
           value={email}
@@ -56,6 +62,7 @@ function Form() {
           type="email"
           placeholder="What's your email?"
         />
+        <h4 className="text-light mt-4">Message:</h4>
         <input
           className="fs-5 border border-info p-2"
           value={message}
@@ -75,7 +82,7 @@ function Form() {
       </form>
       {errorMessage && (
         <div>
-          <p className="error-text text-warning">{errorMessage}</p>
+          <p className="error-text text-danger fs-5">{errorMessage}</p>
         </div>
       )}
     </div>
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,18 +5,24 @@ import './assets/styles/Hero.css';
 function Navigation({ currentPage, handlePageChange }) {
   return (
     <header>
-      <Navbar bg="dark" variant="dark" expand="lg">
-        <div className="navbar-brand active mx-3 p-3 fs-3 text-info">PORTFOLIO</div>
-        <Container>
-          <Navbar.Toggle aria-controls="navbar-nav" />
-          <Navbar.Collapse className="justify-content-end" id="navbar-nav">
-            <Nav className="nav color fs-5 m-3 p-2">
+      <Navbar
+        bg="dark"
+        variant="dark"
+        expand="lg">
+        <div
+          className="navbar-brand active mx-3 p-3 fs-1 text-info">PORTFOLIO</div>
+        <Container fluid>
+          <Navbar.Toggle
+            aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse
+            className="justify-content-end" id="responsive-navbar-nav">
+            <Nav
+              className="nav color fs-3 m-3 p-2">
               <Nav.Link
                 href="#about"
                 onClick={() => handlePageChange('About')}
                 className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
                 className="mx-2"
-
               >
                 About
               </Nav.Link>
@@ -33,7 +39,6 @@ function Navigation({ currentPage, handlePageChange }) {
                 onClick={() => handlePageChange('Contact')}
                 className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
                 className="mx-2"
-
               >
                 Contact
               </Nav.Link>
@@ -42,7 +47,6 @@ function Navigation({ currentPage, handlePageChange }) {
                 onClick={() => handlePageChange('Resume')}
                 className="nav-link"
                 className="mx-2"
-
               >
                 Resume
               </Nav.Link>
@@ -50,11 +54,15 @@ function Navigation({ currentPage, handlePageChange }) {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      <div className="position-relative overflow-hidden p-3 p-md-5 text-start" id="hero">
-        <div className="col-md-5 p-lg-5">
-          <h1 className="text-light text-left fw-bold">Matt Holt Moore</h1>
+      <div
+        className="position-relative overflow-hidden p-3 p-md-5 text-start" id="hero">
+        <div
+          className="col-md-5 p-lg-5">
+          <h1
+            className="text-light text-left fw-bold">Matt Holt Moore</h1>
         </div>
-        <div className="product-device product-device-2 shadow-sm d-none d-md-block"></div>
+        <div
+          className="product-device product-device-2 shadow-sm d-none d-md-block"></div>
       </div>
     </header >
   );
